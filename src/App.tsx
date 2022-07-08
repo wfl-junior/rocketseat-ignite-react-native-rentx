3a76637fd6567c69e99427f78d339f915449f507
Wallace Júnior <wflj1997@hotmail.com>
2022-07-08 19:04:05 -0300

    Criação do projeto e dica ninja

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,4 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Home } from "./screens/Home";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 28,
-  },
-});
-
-export const App: React.FC = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}>Hello World</Text>
-  </View>
-);
+export const App: React.FC = () => <Home />;
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Title } from "./styles";
+
+interface HomeProps {}
+
+export const Home: React.FC<HomeProps> = () => {
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  );
+};
