60f3e819dfa8b5da5cafa61ad44e72af34109d2a
Wallace Júnior <wflj1997@hotmail.com>
2022-07-09 14:02:12 -0300

    Criação do Header da Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,28 @@
-import { Container, Title } from "./styles";
+import { Fragment } from "react";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import Logo from "../../assets/logo.svg";
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
 interface HomeProps {}
 
 export const Home: React.FC<HomeProps> = () => {
   return (
-    <Container>
-      <Title>Home</Title>
-    </Container>
+    <Fragment>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Container>
+        <Header>
+          <HeaderContent>
+            <Logo width={RFValue(108)} height={RFValue(12)} />
+            <TotalCars>Total de 12 carros</TotalCars>
+          </HeaderContent>
+        </Header>
+      </Container>
+    </Fragment>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,29 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: ${({ theme }) => theme.colors.background.primary};
 `;
 
-export const Title = styled.Text`
-  font-size: 30px;
-  font-family: ${({ theme }) => theme.fonts.secondary[600]};
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(113)}px;
+  background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+  padding: 0 ${RFValue(24)}px ${RFValue(28)}px;
+`;
+
+export const HeaderContent = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary[400]};
+  color: ${({ theme }) => theme.colors.text.DEFAULT};
 `;
