845835e71e5deea9fd94d2f9bd379fd0a263f292
Wallace Júnior <wflj1997@hotmail.com>
2022-07-11 13:22:13 -0300

    Obtendo a imagem em foco

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import { FlatList } from "react-native";
+import { useRef, useState } from "react";
+import { FlatList, ViewToken } from "react-native";
 import { CarDTO } from "../../dtos/CarDTO";
 import {
   CarImage,
@@ -8,30 +9,44 @@ import {
   ImageIndexes,
 } from "./styles";
 
+interface ViewableItemsChangedInfo {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 interface ImageSliderProps {
   photos: CarDTO["photos"];
 }
 
-export const ImageSlider: React.FC<ImageSliderProps> = ({ photos: images }) => (
-  <Container>
-    <ImageIndexes>
-      {images.map((image, index) => (
-        <ImageIndex key={image} active={index === 0} />
-      ))}
-    </ImageIndexes>
+export const ImageSlider: React.FC<ImageSliderProps> = ({ photos: images }) => {
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
 
-    <CarImageWrapper>
-      <FlatList
-        data={images}
-        keyExtractor={image => image}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        renderItem={({ item: image }) => (
-          <CarImageWrapper>
-            <CarImage source={{ uri: image }} resizeMode="contain" />
-          </CarImageWrapper>
-        )}
-      />
-    </CarImageWrapper>
-  </Container>
-);
+  const handleIndexChange = useRef((info: ViewableItemsChangedInfo) => {
+    setActiveImageIndex(info.viewableItems[0].index!);
+  });
+
+  return (
+    <Container>
+      <ImageIndexes>
+        {images.map((image, index) => (
+          <ImageIndex key={image} active={index === activeImageIndex} />
+        ))}
+      </ImageIndexes>
+
+      <CarImageWrapper>
+        <FlatList
+          data={images}
+          keyExtractor={image => image}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          onViewableItemsChanged={handleIndexChange.current}
+          renderItem={({ item: image }) => (
+            <CarImageWrapper>
+              <CarImage source={{ uri: image }} resizeMode="contain" />
+            </CarImageWrapper>
+          )}
+        />
+      </CarImageWrapper>
+    </Container>
+  );
+};
