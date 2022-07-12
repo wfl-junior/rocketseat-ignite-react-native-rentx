02c71e101765dd77ae734025987145121f365811
Wallace Júnior <wflj1997@hotmail.com>
2022-07-12 18:37:05 -0300

    Persistindo dados

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -5,6 +5,8 @@ import {
   useEffect,
   useState,
 } from "react";
+import { database } from "../database";
+import { User } from "../database/models/User";
 import { UserDTO } from "../dtos/UserDTO";
 import { api } from "../services/api";
 
@@ -35,21 +37,51 @@ interface AuthContextProviderProps {
 export const AuthContextProvider: React.FC<AuthContextProviderProps> = ({
   children,
 }) => {
-  const [token, setToken] = useState<string | null>(null);
   const [user, setUser] = useState<UserDTO | null>(null);
 
   useEffect(() => {
-    (api.defaults.headers as any).Authorization = `Bearer ${token}`;
-  }, [token]);
+    const userCollection = database.get<User>("users");
+
+    userCollection
+      .query()
+      .fetch()
+      .then(users => {
+        if (users.length) {
+          const user = users[0]._raw as unknown as User;
+          (api.defaults.headers as any).Authorization = `Bearer ${user.token}`;
+
+          setUser({
+            id: user.user_id,
+            name: user.name,
+            email: user.email,
+            driver_license: user.driver_license,
+            avatar: user.avatar,
+          });
+        }
+      })
+      .catch(console.warn);
+  }, []);
 
   const signIn: AuthContextData["signIn"] = useCallback(async credentials => {
-    const { data } = await api.post<SignInResponseData>(
-      "/sessions",
-      credentials,
-    );
+    const {
+      data: { token, user },
+    } = await api.post<SignInResponseData>("/sessions", credentials);
 
-    setToken(data.token);
-    setUser(data.user);
+    const userCollection = database.get<User>("users");
+
+    await database.write(async () => {
+      await userCollection.create(newUser => {
+        newUser.user_id = user.id;
+        newUser.name = user.name;
+        newUser.email = user.email;
+        newUser.driver_license = user.driver_license;
+        newUser.avatar = user.avatar;
+        newUser.token = token;
+      });
+    });
+
+    (api.defaults.headers as any).Authorization = `Bearer ${token}`;
+    setUser(user);
   }, []);
 
   return (
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useState } from "react";
 import {
   Alert,
   Keyboard,
@@ -11,7 +11,6 @@ import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { PasswordInput } from "../../components/PasswordInput";
 import { useAuthContext } from "../../contexts/AuthContext";
-import { database } from "../../database";
 import { useAuthNavigation } from "../../hooks/useAuthNavigation";
 import { theme } from "../../styles/theme";
 import { Container, Footer, Form, Header, SubTitle, Title } from "./styles";
@@ -30,11 +29,6 @@ export const SignIn: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { signIn } = useAuthContext();
-
-  useEffect(() => {
-    const userCollection = database.get("users");
-    userCollection.query().fetch().then(console.log).catch(console.warn);
-  }, []);
 
   async function handleSignIn() {
     setIsLoading(true);
