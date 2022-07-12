ad09ef2004c313c32c164ea5f1e54cd9c31bed92
Wallace Júnior <wflj1997@hotmail.com>
2022-07-12 17:33:05 -0300

    Configuração do banco no Android

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,9 @@ module.exports = function (api) {
 
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
